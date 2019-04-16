defe07667476ef2a005395f1d861a5d88a7058b6
Dominique Robinson <domrobinson.dj@gmail.com>
2019-04-15 22:26:55 -0500

    updating structure

--- app/index.js
+++ app/index.js
@@ -1,11 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
-
-class App extends React.Component {
-  render() {
-    return <div>Hello Matt</div>;
-  }
-}
+var App = require('./components/App');
 
 ReactDOM.render(<App />, document.getElementById('app'));
